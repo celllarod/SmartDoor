5dc4275bda2d218d6ef413a0e7fefdaa4b52ab39
celllarod <llanes.celia@icloud.com>
2023-04-01 10:12:02 +0200

    feat: app móvil

--- app/app/src/main/java/com/motosharing/smartdoorapp/MainActivity.java
+++ app/app/src/main/java/com/motosharing/smartdoorapp/MainActivity.java
@@ -1,14 +1,52 @@
 package com.motosharing.smartdoorapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.motosharing.smartdoorapp.api.ApiAdapter;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt_open;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_open = findViewById(R.id.bt_open);
+        bt_open.setOnClickListener(view -> openDoor(view));
+
+
+    }
+
+    private void openDoor(View v) {
+        Call<Boolean> call = ApiAdapter.getApiService().openDoor();
+        call.enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(@NonNull Call<Boolean> call, @NonNull Response<Boolean> response) {
+                if (response.isSuccessful()) {
+                    Log.d("OPEN", response.toString());
+                } else {
+
+                }
+            }
+            @Override
+            public void onFailure(@NonNull Call<Boolean> call, @NonNull Throwable t) {
+                Log.d("ERROR", t.toString());
+            }
+        });
     }
 }
---
+++ app/app/src/main/java/com/motosharing/smartdoorapp/api/ApiAdapter.java
@@ -0,0 +1,36 @@
+package com.motosharing.smartdoorapp.api;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiAdapter {
+
+    private static ApiService API_SERVICE;
+    private static final String HOST = "192.168.160.223";
+    private static final String BASE_URL = "http:/" + HOST +":5000/";
+
+    public static ApiService getApiService() {
+
+        // Creamos un interceptor y le indicamos el log level a usar
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        // Asociamos el interceptor a las peticiones
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(logging);
+
+        if (API_SERVICE == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient.build())
+                    .build();
+            API_SERVICE = retrofit.create(ApiService.class);
+        }
+
+        return API_SERVICE;
+    }
+}
+
